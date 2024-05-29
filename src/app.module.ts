832475049138435323a9f06f5130022976570f6e
ragacijane <lukajanke23@gmail.com>
2024-05-29 19:43:07 +0200

    started: true

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { KandidatiModule } from './kandidati/kandidati.module';
+import { KorisniciModule } from './korisnici/korisnici.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3308,
+    username: 'root',
+    password: 'majeskuel123',
+    database: 'bazakandidata',
+    synchronize: true,
+    logging: true,
+    entities: [__dirname + '/**/*.entity{.ts,.js}']
+  }), KandidatiModule, KorisniciModule, AuthModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
